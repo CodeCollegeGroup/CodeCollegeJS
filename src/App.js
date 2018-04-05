c5ac94fd939edef5f898554635e8b2a957e77002
Vinicius Ferreira <vfbdelima@gmail.com>
2018-04-04 23:55:19 -0300

    Update material ui dependency
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>
    Signed-off-by: João Robson <joaorobsonmartins@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './stylesheet/App.sass';
+import Header from './layout/Header';
+import SidebarMenu from './layout/SidebarMenu';
+import Routers from './routes/Routers';
+import AppStore from './stores/AppStore';
+import AppDispatcher from './AppDispatcher';
+import ActionType from './actions/ActionType';
+import {Auth} from './auth/Auth';
+import LoggerStore from './stores/LoggerStore';  // eslint-disable-line no-unused-vars
+import Snackbar from 'material-ui/Snackbar';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = AppStore.getState();
+  }
+
+  componentWillMount = () => {
+    this.setState({
+      listener: AppStore.addListener(this.handleChange)
+    });
+
+    /* Add validation to logout when user don not make some moviment */
+    const body = document.getElementsByTagName('body')[0];
+    body.onmousemove = Auth.updateDate;
+    body.onkeyup = Auth.updateDate;
+    Auth.autoLogin();
+  }
+
+  componentWillUnmout = () => this.state.listener.remove()
+
+  handleChangeRequestNavDrawer = () => AppDispatcher.dispatch({
+    action: ActionType.APP.SWITCHNAVDRAWER,
+  })
+
+  handleToggle = () => AppDispatcher.dispatch({
+    action: ActionType.APP.MENUTOGGLE, })
+
+  handleChange = () => this.setState(AppStore.getState())
+
+  render = () => {
+    const { navDrawerOpen } = this.state;
+    const paddingLeftDrawerOpen = 250;
+
+    const styles = {
+      header: {
+        paddingLeft: navDrawerOpen ? paddingLeftDrawerOpen : 0
+      },
+      container: {
+        paddingLeft: navDrawerOpen ? paddingLeftDrawerOpen : 0,
+        margin: navDrawerOpen ? '90px 20px 20px 15px' : '90px 5% 0px 8%'
+      }
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header styles={styles.header}
+          handleChangeRequestNavDrawer={this.handleChangeRequestNavDrawer}
+          auth={this.state.auth}
+        />
+        <SidebarMenu auth={this.state.auth} navDrawerOpen={this.state.navDrawerOpen} />
+        <div style={styles.container}>
+          <Routers />
+          <Snackbar
+            open={this.state.snackOpen}
+            message={this.state.snackMessage}
+            autoHideDuration={9000}
+            action='Ok'
+            onRequestClose={
+              () => AppDispatcher.dispatch({action: ActionType.USERFEEDBACK, snackOpen: false})
+            }
+          />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import theme from './stylesheet/theme';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+      <App />
+    </MuiThemeProvider>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
